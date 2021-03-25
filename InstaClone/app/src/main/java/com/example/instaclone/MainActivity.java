6f4a146e4eab064d12a42588ba455034ae154c9a
stevens34400 <stevensisjayawan34400@gmail.com>
2021-03-25 16:59:04 -0400

    finish unit 5

--- InstaClone/app/src/main/java/com/example/instaclone/MainActivity.java
+++ InstaClone/app/src/main/java/com/example/instaclone/MainActivity.java
@@ -20,11 +20,16 @@ import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -64,8 +69,12 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Description cannot be empty!",Toast.LENGTH_SHORT).show();
                     return;
                 }
+                if (photoFile == null || ivPostImage.getDrawable() == null){
+                    Toast.makeText(MainActivity.this, "There is no image!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 ParseUser currentUser = ParseUser.getCurrentUser();
-                savePost(description,currentUser);
+                savePost(description,currentUser, photoFile);
             }
         });
     }
@@ -99,13 +108,43 @@ public class MainActivity extends AppCompatActivity {
                 Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
                 // RESIZE BITMAP, see section below
                 // See code above
-//                Uri takenPhotoUri = Uri.fromFile(getPhotoFileUri(photoFileName));
+                Uri takenPhotoUri = Uri.fromFile(getPhotoFileUri(photoFileName));
 //                // by this point we have the camera photo on disk
-//                Bitmap rawTakenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+                Bitmap rawTakenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
 //                // See BitmapScaler.java: https://gist.github.com/nesquena/3885707fd3773c09f1bb
-//                Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(rawTakenImage, SOME_WIDTH);
+                Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(rawTakenImage, 1000);
+
+//                // Configure byte output stream
+//                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+//                // Compress the image further
+//                resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+//                // Create a new file for the resized bitmap (`getPhotoFileUri` defined above)
+//                File resizedFile = getPhotoFileUri(photoFileName + "_resized");
+//                try {
+//                    resizedFile.createNewFile();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                FileOutputStream fos = null;
+//                try {
+//                    fos = new FileOutputStream(resizedFile);
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+//                // Write the bytes of the bitmap to file
+//                try {
+//                    fos.write(bytes.toByteArray());
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                try {
+//                    fos.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+
 //                // Load the taken image into a preview
-                ivPostImage.setImageBitmap(takenImage);
+                ivPostImage.setImageBitmap(resizedBitmap);
             } else { // Result was a failure
                 Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
             }
@@ -130,10 +169,11 @@ public class MainActivity extends AppCompatActivity {
         return file;
     }
 
-    private void savePost(String description, ParseUser currentUser) {
+    private void savePost(String description, ParseUser currentUser, File photoFile) {
         Post post = new Post();
         post.setDescription(description);
         post.setUser(currentUser);
+        post.setImage(new ParseFile(photoFile));
         post.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
@@ -144,6 +184,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 Log.i(TAG, "post save successful");
                 etDescription.setText("");
+                ivPostImage.setImageResource(0);
             }
         });
     }
